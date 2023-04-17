f7806e533de79d08d26af699e76f32062012005a
Ed Warnicke <hagbard@gmail.com>
2023-04-17 12:10:20 -0500

    Convert tabs to spaces
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,7 +1,7 @@
 import { FiExternalLink } from "react-icons/fi";
 import {
-	resourcesArticles,
-	resourcesMedia,
+  resourcesArticles,
+  resourcesMedia,
 } from "../data/resourcesList";
 import styles from "../styles/pages/ResourcesPage.module.scss";
 import { IResourcesArticle, IResourcesMedia } from "../types/types";
@@ -9,116 +9,116 @@ import CustomImage from "../components/reusables/CustomImage";
 import Layout from "../components/Layout";
 
 const Resources = () => {
-	return (
-		<Layout pagePath="/resources" pageTitle="Resources">
-			<div className={styles.Resources}>
-						<HeaderSection />
-				<div className={styles.ResourcesColumnContainer}>
-					<div className={styles.ResourcesLeftContainer}>
-						<div className={styles.ResourcesArticleGrid}>
-							{resourcesArticles.map((article, i) => {
-								return (
-									<ResourcesArticle
-										image={article.image}
-										title={article.title.length > 70 ? `${article.title.slice(0, 71)}...` : article.title}
-										link={article.link}
-										linkText={article.linkText}
-										author={article.author}
-										key={i}
-									/>
-								);
-							})}
-						</div>
-					</div>
-					<div className={styles.ResourcesDivider} />
-					<div className={styles.ResourcesRightContainer}>
-						{resourcesMedia.map((media, i) => {
-							return (
-								<ResourcesMedia
-									image={media.image}
-									link={media.link}
-									mediaType={media.mediaType}
-									title={media.title}
-									key={i}
-								/>
-							);
-						})}
-					</div>
-				</div>
-			</div>
-		</Layout>
-	);
+  return (
+    <Layout pagePath="/resources" pageTitle="Resources">
+      <div className={styles.Resources}>
+            <HeaderSection />
+        <div className={styles.ResourcesColumnContainer}>
+          <div className={styles.ResourcesLeftContainer}>
+            <div className={styles.ResourcesArticleGrid}>
+              {resourcesArticles.map((article, i) => {
+                return (
+                  <ResourcesArticle
+                    image={article.image}
+                    title={article.title.length > 70 ? `${article.title.slice(0, 71)}...` : article.title}
+                    link={article.link}
+                    linkText={article.linkText}
+                    author={article.author}
+                    key={i}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles.ResourcesDivider} />
+          <div className={styles.ResourcesRightContainer}>
+            {resourcesMedia.map((media, i) => {
+              return (
+                <ResourcesMedia
+                  image={media.image}
+                  link={media.link}
+                  mediaType={media.mediaType}
+                  title={media.title}
+                  key={i}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 const HeaderSection = () => {
-	return (
-		<div className={styles.HeaderSection}>
-			<h1 className={styles.HeaderSectionEyebrow}>Resources</h1>
-			<h2>Learn more about <br />OpenClarity</h2>
-			<p className={styles.HeaderSectionShortText}>Check out the latest blog articles, podcasts, videos, and more from the OpenClarity community.</p>
-		</div>
-	)
+  return (
+    <div className={styles.HeaderSection}>
+      <h1 className={styles.HeaderSectionEyebrow}>Resources</h1>
+      <h2>Learn more about <br />OpenClarity</h2>
+      <p className={styles.HeaderSectionShortText}>Check out the latest blog articles, podcasts, videos, and more from the OpenClarity community.</p>
+    </div>
+  )
 }
 
 const ResourcesArticle = ({
-	image,
-	title,
-	link,
-	linkText,
-	author,
+  image,
+  title,
+  link,
+  linkText,
+  author,
 }: IResourcesArticle) => {
-	return (
-		<a href={link} target="_blank" className={styles.ResourcesArticle}>
-				<img src={image} alt={title}  />
-			<div className={styles.ResourcesArticleContentContainer}>
-				<a
-					className={styles.ResourcesLink}
-					href={link}
-					target="_blank"
-				>
-					<FiExternalLink className={styles.ResourcesLinkIcon} />
-					<p>Article: {linkText}</p>
-				</a>
-				<h6 className={styles.ResourcesArticleTitle}>{title}</h6>
-				<p className={styles.ResourcesArticleAuthor}>
-					by {author}
-				</p>
-			</div>
-		</a>
-	);
+  return (
+    <a href={link} target="_blank" className={styles.ResourcesArticle}>
+        <img src={image} alt={title}  />
+      <div className={styles.ResourcesArticleContentContainer}>
+        <a
+          className={styles.ResourcesLink}
+          href={link}
+          target="_blank"
+        >
+          <FiExternalLink className={styles.ResourcesLinkIcon} />
+          <p>Article: {linkText}</p>
+        </a>
+        <h6 className={styles.ResourcesArticleTitle}>{title}</h6>
+        <p className={styles.ResourcesArticleAuthor}>
+          by {author}
+        </p>
+      </div>
+    </a>
+  );
 };
 
 const ResourcesMedia = ({
-	image,
-	mediaType,
-	link,
-	title,
+  image,
+  mediaType,
+  link,
+  title,
 }: IResourcesMedia) => {
-	return (
-		<a href={link} target="_blank" className={styles.ResourcesMedia}>
-			<div
-				className={styles.ResourcesMediaImage
-				}
-			>
-				<CustomImage
-					src={image}
-					alt={title}
-					lazyLoad={false}
-				/>
-			</div>
-			<div className={styles.ResourcesMediaContent}>
-				<a
-					className={styles.ResourcesLink}
-					href={link}
-					target="_blank"
-				>
-					<FiExternalLink className={styles.ResourcesLinkIcon} />
-					<p>{mediaType}</p>
-				</a>
-				<p className={styles.ResourcesMediaTitle}>{title.length > 70 ? `${title.slice(0, 70)}...` : title}</p>
-			</div>
-		</a>
-	);
+  return (
+    <a href={link} target="_blank" className={styles.ResourcesMedia}>
+      <div
+        className={styles.ResourcesMediaImage
+        }
+      >
+        <CustomImage
+          src={image}
+          alt={title}
+          lazyLoad={false}
+        />
+      </div>
+      <div className={styles.ResourcesMediaContent}>
+        <a
+          className={styles.ResourcesLink}
+          href={link}
+          target="_blank"
+        >
+          <FiExternalLink className={styles.ResourcesLinkIcon} />
+          <p>{mediaType}</p>
+        </a>
+        <p className={styles.ResourcesMediaTitle}>{title.length > 70 ? `${title.slice(0, 70)}...` : title}</p>
+      </div>
+    </a>
+  );
 };
 
 
